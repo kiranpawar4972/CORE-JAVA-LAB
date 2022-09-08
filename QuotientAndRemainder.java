77150896be44d1371dad94eed0af3aede8b2c633
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2022-09-08 17:18:57 +0530

    Add files via upload

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,18 @@
+// find Quotient And Remainder
+
+class QuotientAndRemainder 
+{
+ 
+    public static void main(String args[])
+    {
+ 
+        int dividend = 99;
+		int	divisor = 11;
+ 
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+ 
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+}
