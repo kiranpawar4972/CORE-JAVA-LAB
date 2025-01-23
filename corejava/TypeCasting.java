9d6e6193c295e46162d0b3e34fff5502e523ef58
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2025-01-23 14:55:57 +0530

    Add files via upload

---
+++ corejava/TypeCasting.java
@@ -0,0 +1,19 @@
+package corejava;
+
+class TypeCasting
+{
+	public static void main(String args[])
+	{
+		/* int a=10;
+		float b=20.8f;
+		b=a;    		// implicit typecasting
+		System.out.println(b);*/
+		
+		double a=34.6;
+		int b=29;
+		b=(int)a;       // explicit typecasting
+		System.out.println(b);
+		
+		
+	}
+}
