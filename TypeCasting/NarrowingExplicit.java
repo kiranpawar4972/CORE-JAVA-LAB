850ec2a35af9312c469e428540932ade11f21407
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2025-08-05 14:34:54 +0530

    Add files via upload

---
+++ TypeCasting/NarrowingExplicit.java
@@ -0,0 +1,23 @@
+package com.typecasting;
+
+public class NarrowingExplicit 
+{
+
+	public static void main(String[] args) 
+	{
+		double v1=44.35576879;
+		System.out.println(v1);
+		
+		float v2=(float)v1;
+		System.out.println(v2);
+		
+		long v3=(long)v1;
+		System.out.println(v3);
+		
+		int v4=97;
+		char v5=(char)v4;
+		System.out.println(v5);
+
+	}
+
+}
---
+++ TypeCasting/WideningImplicit.java
@@ -0,0 +1,24 @@
+package com.typecasting;
+
+public class WideningImplicit 
+{
+
+	public static void main(String[] args) 
+	{
+		//widening/implicit type casting
+		
+		byte age=18;
+		int data=age;
+		System.out.println(data);
+		
+		char v1='A';
+		int v2=v1;
+		System.out.println(v2);
+		
+		int v3=55;
+		float v4=v3;
+		System.out.println(v4);
+
+	}
+
+}
