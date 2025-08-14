2860d73d6905ebe32f6230882fa6d58616cc7e3a
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2025-08-14 11:26:05 +0530

    Add files via upload

---
+++ inheritance/CarBmwMain.java
@@ -0,0 +1,17 @@
+package com.inheritance;
+
+public class CarBmwMain 
+{
+
+	public static void main(String[] args) 
+	{
+		Bmw1 b=new Bmw1();
+		
+		b.speed();
+		b.milage();
+		b.color();
+		b.automode();
+
+	}
+
+}
