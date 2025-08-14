e1c65e797c69816c71e50175520b24b2d5835c62
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2025-08-14 11:21:46 +0530

    Add files via upload

---
+++ inheritance/Car1.java
@@ -0,0 +1,18 @@
+package com.inheritance;
+
+public class Car1 
+{
+	public void speed()
+	{
+		System.out.println("car speed is 80km/h");
+	}
+	public void milage()
+	{
+		System.out.println("car milage is 10km/l");
+	}
+	public void color()
+	{
+		System.out.println("car color is blue");
+	}
+
+}
