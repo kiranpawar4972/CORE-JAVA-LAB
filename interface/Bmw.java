77a36fac44cb5f06605e98ccb052de45dd583521
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2025-08-18 11:21:20 +0530

    Add files via upload

---
+++ interface/Bmw.java
@@ -0,0 +1,22 @@
+package com.interfacee;
+
+public class Bmw implements MyInterface
+{
+	public void speed()
+	{
+		System.out.println("Bmw Speed");
+	}
+	public void milage()
+	{
+		System.out.println("Bmw milage");
+	}
+	public void engine()
+	{
+		System.out.println("Bmw Engine");
+	}
+	public void color()
+	{
+		System.out.println("Bmw Color");
+	}
+	
+}
