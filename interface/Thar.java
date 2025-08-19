3a9d4a2774cfa32e639ad0cb2a5104cdbcf9a65c
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2025-08-19 12:27:47 +0530

    Add files via upload

---
+++ interface/Thar.java
@@ -0,0 +1,22 @@
+package com.interfacee;
+
+public class Thar implements MyInterface
+{
+	public void speed()
+	{
+		System.out.println("Thar Speed");
+	}
+	public void milage()
+	{
+		System.out.println("Thar milage");
+	}
+	public void engine()
+	{
+		System.out.println("Thar Engine");
+	}
+	public void color()
+	{
+		System.out.println("Thar Color");
+	}
+	
+}
