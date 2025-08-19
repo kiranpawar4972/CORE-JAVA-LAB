71b024fa9b2c63fac2c1a3c9de05d13b0179c377
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2025-08-19 11:38:10 +0530

    Add files via upload

---
+++ interface/Curve.java
@@ -0,0 +1,22 @@
+package com.interfacee;
+
+public class Curve implements MyInterface
+{
+	public void speed()
+	{
+		System.out.println("Curve Speed");
+	}
+	public void milage()
+	{
+		System.out.println("Curve milage");
+	}
+	public void engine()
+	{
+		System.out.println("Curve Engine");
+	}
+	public void color()
+	{
+		System.out.println("Curve Color");
+	}
+	
+}
