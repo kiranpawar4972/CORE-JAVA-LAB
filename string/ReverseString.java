2e1586d7b4baec674567b0b679d9300870422bcf
kiranpawar4972 <111423025+kiranpawar4972@users.noreply.github.com>
2025-08-30 11:18:30 +0530

    Add files via upload

---
+++ string/ReverseString.java
@@ -0,0 +1,23 @@
+package com.string;
+
+public class ReverseString 
+{
+
+	public static void main(String[] args) 
+	{
+		String s1="malayalam";
+		String s2="";
+		
+		for(int i=s1.length()-1;i>=0;i--)
+		{
+			s2=s2+s1.charAt(i);
+		}
+		
+		if(s1.equals(s2))
+			System.out.println("given String is palindrome");
+		else
+			System.out.println("given String is not palindrome");
+
+	}
+
+}
